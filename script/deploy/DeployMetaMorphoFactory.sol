54c841ab8d372caa1806947cf3f1b0efafbe1495
phuccaliber <phuc@caliber.build>
2025-05-13 15:51:18 +0700

    feat(build): redeploy metamorpho with correct morpho-blue address

--- script/deploy/DeployMetaMorphoFactory.sol
+++ script/deploy/DeployMetaMorphoFactory.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import "../../lib/metamorpho-v1.1/src/MetaMorphoV1_1Factory.sol";
+import "../Config.sol";
 // import "@metamorpho/MetaMorphoV1_1Factory.sol";
 
 contract DeployMetaMorpho is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("TESTNET_DEPLOYER");
-        address deployer = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
-        MetaMorphoV1_1Factory factory = new MetaMorphoV1_1Factory(deployer);
+        MetaMorphoV1_1Factory factory = new MetaMorphoV1_1Factory(Config.MORPHO_BLUE);
         console.log("Factory deployed at:", address(factory));
 
         vm.stopBroadcast();
