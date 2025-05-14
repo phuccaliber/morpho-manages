b072f1200ddbb1fe480935d228029c99aa5e34b3
phuccaliber <phuc@caliber.build>
2025-05-14 11:05:59 +0700

    feat(contract): add contract OWUSDC

---
+++ src/OWUSDC.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract OWUSDC is ERC20, Ownable {
+    constructor() ERC20("OWUSDC", "OWUSDC") Ownable(msg.sender) {}
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
