16c7ca6378ef363ac8bde7ad143183e0498e510e
phuccaliber <phuc@caliber.build>
2025-05-14 15:59:27 +0700

    feat(script): add scripts to deploy and set curator for vault

--- script/Config.sol
+++ script/Config.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.13;
 /// @notice Contains the addresses of deployed contracts
 library Config {
     // Core protocol
+    address internal constant OWNER = address(0x787bb22b6dEb95D8d61C6628a97448b6fedCb414);
     address internal constant MORPHO_BLUE = address(0xEB4162C6E363e7C925395E82a8fe7BE78bc74A5f);
     address internal constant META_MORPHO_FACTORY = address(0xcB966E4924cFe446cD78720c0453e13C28e94f88);
     address internal constant OWBTC = address(0xc7Bd64978a3dC064fe749F05FeC070b939CFaf07);
     address internal constant OWUSDC = address(0x9a00a26415Bf174cA2D74Cf02d9693d27A3A67a5);
 
-    // Governance
-    address internal constant TIMELOCK = address(0); // Replace with actual address after deployment
-    address internal constant OWNER = address(0); // Replace with actual address after deployment
+    // Vault management
+    address internal constant OWUSDC_VAULT = address(0x76b1a103d58dC2873f80ccaf96b7790668aEB0aC);
     address internal constant FEE_RECIPIENT = address(0); // Replace with actual address after deployment
 
     // Markets
